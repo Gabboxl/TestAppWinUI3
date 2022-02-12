1fb3b4a1da3fb4a246591e6130f5fcb6333990d4
Gabriele <gabboxl0@gmail.com>
2022-02-12 19:15:18 +0100

    aggiunte pagine di test e navigationview funzionante yayy

--- yoyo/MainWindow.xaml.cs
+++ yoyo/MainWindow.xaml.cs
@@ -1,23 +1,18 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Core;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace yoyo
 {
+   
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
@@ -28,26 +23,186 @@ namespace yoyo
             this.InitializeComponent();
 
 
-            myButton.Click += new RoutedEventHandler(async void (object sender, RoutedEventArgs e) => {
 
-                // Call app specific code to subscribe to the service. For example:
-                ContentDialog subscribeDialog = new ContentDialog
-                {
-                    Title = "Esperienza?",
-                    Content = "Salve come va",
-                    CloseButtonText = "No",
-                    PrimaryButtonText = "Okay",
-                    SecondaryButtonText = "forse",
-                    DefaultButton = ContentDialogButton.Primary,
 
-                };
 
-                //fix altrimenti non si vede il dialog perchè non è fissato ad alcun layout radice xaml
-                subscribeDialog.XamlRoot = this.Content.XamlRoot;
-                ContentDialogResult result = await subscribeDialog.ShowAsync();
+           NavView.Loaded += new RoutedEventHandler((object sender, RoutedEventArgs e) => { NavView_Loaded(sender, e); });
+            NavView.ItemInvoked += new Windows.Foundation.TypedEventHandler<NavigationView, NavigationViewItemInvokedEventArgs>((NavigationView sender, NavigationViewItemInvokedEventArgs args) => { NavView_ItemInvoked(sender, args); });
+            NavView.BackRequested += new Windows.Foundation.TypedEventHandler<NavigationView, NavigationViewBackRequestedEventArgs>((NavigationView sender, NavigationViewBackRequestedEventArgs args) => { NavView_BackRequested(sender, args); });
 
-            });
+
         }
+
+
+
+
+
+
+        // Add "using" for WinUI controls.
+        // using muxc = Microsoft.UI.Xaml.Controls;
+
+        private double NavViewCompactModeThresholdWidth { get { return NavView.CompactModeThresholdWidth; } }
+
+        private void ContentFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        // List of ValueTuple holding the Navigation Tag and the relative Navigation Page
+        private readonly List<(string Tag, Type Page)> _pages = new List<(string Tag, Type Page)>
+{
+    ("home", typeof(BlankPage1)),
+    ("apps", typeof(BlankPage2)),
+    ("games", typeof(BlankPage3)),
+    ("music", typeof(BlankPage4)),
+};
+
+        private void NavView_Loaded(object sender, RoutedEventArgs e)
+        {
+            // You can also add items in code.
+            NavView.MenuItems.Add(new NavigationViewItemSeparator());
+            NavView.MenuItems.Add(new NavigationViewItem
+            {
+                Content = "My content",
+                Icon = new SymbolIcon((Symbol)0xF1AD),
+                Tag = "content"
+            });
+            _pages.Add(("content", typeof(MainWindow)));
+
+            // Add handler for ContentFrame navigation.
+            Frame1.Navigated += On_Navigated;
+
+            // NavView doesn't load any page by default, so load home page.
+            NavView.SelectedItem = NavView.MenuItems[0];
+            // If navigation occurs on SelectionChanged, this isn't needed.
+            // Because we use ItemInvoked to navigate, we need to call Navigate
+            // here to load the home page.
+            NavView_Navigate("home", new Microsoft.UI.Xaml.Media.Animation.EntranceNavigationTransitionInfo());
+        }
+
+        private void NavView_ItemInvoked(NavigationView sender,
+                                         NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked == true)
+            {
+                NavView_Navigate("settings", args.RecommendedNavigationTransitionInfo);
+            }
+            else if (args.InvokedItemContainer != null)
+            {
+                var navItemTag = args.InvokedItemContainer.Tag.ToString();
+                NavView_Navigate(navItemTag, args.RecommendedNavigationTransitionInfo);
+            }
+        }
+
+        // NavView_SelectionChanged is not used in this example, but is shown for completeness.
+        // You will typically handle either ItemInvoked or SelectionChanged to perform navigation,
+        // but not both.
+        private void NavView_SelectionChanged(NavigationView sender,
+                                              NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.IsSettingsSelected == true)
+            {
+                NavView_Navigate("settings", args.RecommendedNavigationTransitionInfo);
+            }
+            else if (args.SelectedItemContainer != null)
+            {
+                var navItemTag = args.SelectedItemContainer.Tag.ToString();
+                NavView_Navigate(navItemTag, args.RecommendedNavigationTransitionInfo);
+            }
+        }
+
+        private void NavView_Navigate(
+            string navItemTag,
+            Microsoft.UI.Xaml.Media.Animation.NavigationTransitionInfo transitionInfo)
+        {
+            Type _page = null;
+            if (navItemTag == "settings")
+            {
+                _page = typeof(SettingsPage);
+            }
+            else
+            {
+                var item = _pages.FirstOrDefault(p => p.Tag.Equals(navItemTag));
+                _page = item.Page;
+            }
+            // Get the page type before navigation so you can prevent duplicate
+            // entries in the backstack.
+            var preNavPageType = Frame1.CurrentSourcePageType;
+
+            // Only navigate if the selected page isn't currently loaded.
+            if (!(_page is null) && !Type.Equals(preNavPageType, _page))
+            {
+                Frame1.Navigate(_page, null, transitionInfo);
+            }
+        }
+
+        private void NavView_BackRequested(NavigationView sender,
+                                           NavigationViewBackRequestedEventArgs args)
+        {
+            TryGoBack();
+        }
+
+        private void DispatcherQueue_AcceleratorKeyActivated(DispatcherQueue sender, AcceleratorKeyEventArgs e)
+        {
+            // When Alt+Left are pressed navigate back
+            if (e.EventType == CoreAcceleratorKeyEventType.SystemKeyDown
+                && e.VirtualKey == VirtualKey.Left
+                && e.KeyStatus.IsMenuKeyDown == true
+                && !e.Handled)
+            {
+                e.Handled = TryGoBack();
+            }
+        }
+
+
+        private void CoreWindow_PointerPressed(CoreWindow sender, PointerEventArgs e)
+        {
+            // Handle mouse back button.
+            if (e.CurrentPoint.Properties.IsXButton1Pressed)
+            {
+                e.Handled = TryGoBack();
+            }
+        }
+
+        private bool TryGoBack()
+        {
+            if (!Frame1.CanGoBack)
+                return false;
+
+            // Don't go back if the nav pane is overlayed.
+            if (NavView.IsPaneOpen &&
+                (NavView.DisplayMode == NavigationViewDisplayMode.Compact ||
+                 NavView.DisplayMode == NavigationViewDisplayMode.Minimal))
+                return false;
+
+            Frame1.GoBack();
+            return true;
+        }
+
+        private void On_Navigated(object sender, NavigationEventArgs e)
+        {
+            NavView.IsBackEnabled = Frame1.CanGoBack;
+
+            if (Frame1.SourcePageType == typeof(SettingsPage))
+            {
+                // SettingsItem is not part of NavView.MenuItems, and doesn't have a Tag.
+                NavView.SelectedItem = (NavigationViewItem)NavView.SettingsItem;
+                NavView.Header = "Settings";
+            }
+            else if (Frame1.SourcePageType != null)
+            {
+                var item = _pages.FirstOrDefault(p => p.Page == e.SourcePageType);
+
+                NavView.SelectedItem = NavView.MenuItems
+                    .OfType<NavigationViewItem>()
+                    .First(n => n.Tag.Equals(item.Tag));
+
+                NavView.Header =
+                    ((NavigationViewItem)NavView.SelectedItem)?.Content?.ToString();
+            }
+        }
+
+
 
 
     }
